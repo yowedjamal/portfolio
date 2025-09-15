816bf63b9f102ad2057efd95d470872b0e500caf
Djamal GANI <yowedjamal@gmail.com>
2025-09-15 01:56:57 +0100

    Revert "After intl"
    This reverts commit ab8ca53516fc7e5342eea22406095e3e2348e3be.

--- next.config.js
+++ next.config.js
@@ -1,22 +1,37 @@
 /** @type {import('next').NextConfig} */
-const createNextIntlPlugin = require('next-intl/plugin');
-
-const withNextIntl = createNextIntlPlugin('./i18n.ts');
-
 const nextConfig = {
   // output: 'export',
   images: { 
-    unoptimized: true,
+    unoptimized: false, // Activer l'optimisation des images
+    formats: ['image/webp', 'image/avif'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    minimumCacheTTL: 60 * 60 * 24 * 365, // 1 an
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'miro.medium.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.hashnode.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
     ],
+  },
+  experimental: {
+    optimizePackageImports: ['lucide-react'],
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
 
-module.exports = withNextIntl(nextConfig);
+module.exports = nextConfig;
